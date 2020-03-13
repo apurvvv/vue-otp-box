1d0ec63d304a9832949b97215637128979849503
apurv singh <apurvsingh000@gmail.com>
2020-03-13 17:26:14 +0530

    Changes

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <VueOtp :numberOfBoxes="5" position="left" color="#fe4a49" />
+    </div>
+</template>
+
+<script>
+    import VueOtp from "./VueOtp";
+    export default {
+        name: "App",
+        components: {
+            VueOtp
+        }
+    };
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        width: 100%;
+        color: #2c3e50;
+    }
+</style>
---
+++ src/VueOtp.vue
@@ -0,0 +1,227 @@
+<template>
+    <div id="wrapper" :style="{ justifyContent: direction }">
+        <template v-for="(char, index) in arraySize">
+            <input
+                type="tel"
+                :key="index"
+                maxlength="1"
+                v-model="arraySize[index]"
+                v-on="listeners"
+                @keydown="
+                    handleEnterKey($event);
+                    handleKeyDown($event, index);
+                "
+                @input="handleInput($event, index)"
+                @paste="onPaste"
+                :autofocus="index === 0"
+                :ref="`otp${index}`"
+                @focus="newColor(index)"
+                @blur="defaultColor(index)"
+            />
+        </template>
+    </div>
+</template>
+<script>
+    export default {
+        props: ["numberOfBoxes", "position", "color"],
+
+        data() {
+            return {
+                arraySize: undefined,
+                boxLength: undefined,
+                direction: undefined,
+                boxColor: "#26a69a"
+            };
+        },
+        mounted() {
+            this.handleBoxes();
+
+            if (typeof this.color !== "undefined" && this.color) {
+                this.boxColor = this.color;
+            }
+
+            this.handleAlignment();
+        },
+        methods: {
+            handleBoxes() {
+                if (
+                    typeof this.numberOfBoxes == "undefined" &&
+                    !this.numberOfBoxes
+                ) {
+                    this.boxLength = 4;
+                    this.arraySize = Array(this.boxLength).fill("");
+                } else if (typeof this.numberOfBoxes == "number") {
+                    this.boxLength = this.numberOfBoxes;
+                    this.arraySize = Array(this.boxLength).fill("");
+                } else {
+                    this.boxLength = 4;
+                    this.arraySize = Array(this.boxLength).fill("");
+                }
+            },
+            handleAlignment() {
+                switch (this.position) {
+                    case "left":
+                        this.direction = "flex-start";
+
+                        break;
+                    case "right":
+                        this.direction = "flex-end";
+                        break;
+                    case "center":
+                        this.direction = "center";
+                        break;
+                    default:
+                        this.direction = "center";
+                }
+            },
+            newColor(index) {
+                let i = "otp" + index;
+                this.$refs[
+                    i
+                ][0].style.boxShadow = ` 0 0 5px  ${this.boxColor} inset`;
+                this.$refs[i][0].style.border = `1px solid ${this.boxColor}`;
+            },
+            defaultColor(index) {
+                let i = "otp" + index;
+                this.$refs[i][0].style.boxShadow = " 0 0 5px #ccc inset";
+                this.$refs[i][0].style.border = "solid 1px #ccc";
+            },
+            handleEnterKey(event) {
+                if (event.key === "Enter") {
+                    this.$emit("enter");
+                    event.stopPropagation();
+                }
+            },
+            sanitizeKeyData(key) {
+                return key === "Unidentified" ? undefined : key;
+            },
+            emitInput() {
+                const result = this.arraySize
+                    .join("")
+                    .slice(0, this.numberOfBoxes);
+                this.$emit("input", result);
+            },
+            handleKeyDown(event, index) {
+                const key = this.sanitizeKeyData(event.key);
+                if (!key) {
+                    return;
+                } else if (key === "Backspace") {
+                    if (this.arraySize[index]) {
+                        return (this.arraySize[index] = "");
+                    }
+
+                    if (index > 0) {
+                        event.target.previousElementSibling.focus();
+                    }
+                } else if (
+                    !event.shiftKey &&
+                    (key === "ArrowRight" || key === "Right")
+                ) {
+                    if (index < this.arraySize.length - 1) {
+                        event.target.nextElementSibling.focus();
+                    }
+                } else if (
+                    !event.shiftKey &&
+                    (key === "ArrowLeft" || key === "Left")
+                ) {
+                    if (index > 0) {
+                        event.target.previousElementSibling.focus();
+                    }
+                } else if (key.length === 1 && this.arraySize[index]) {
+                    this.arraySize[index] = key;
+                    this.$forceUpdate();
+                    if (index < this.arraySize.length - 1) {
+                        event.target.nextElementSibling.focus();
+                    }
+                    this.emitInput();
+                }
+            },
+            handleInput(event, index) {
+                const value = this.arraySize[index];
+
+                if (value) {
+                    if (value.length > 1) {
+                        this.arraySize[index] = value[value.length - 1];
+                    }
+                    if (index < this.arraySize.length - 1) {
+                        event.target.nextElementSibling.focus();
+                    }
+                }
+
+                this.emitInput();
+            },
+            onPaste(event) {
+                const clipboardData =
+                    event.clipboardData || window.clipboardData;
+                if (!clipboardData) {
+                    return;
+                }
+                event.preventDefault();
+                const code =
+                    clipboardData.getData("Text") ||
+                    clipboardData.getData("text/plain");
+                this.fillCode(code);
+            },
+            fillCode(code) {
+                code = code.trim();
+                code = code.slice(0, this.numberOfBoxes);
+                const parts = code.split("");
+                parts.length = this.numberOfBoxes;
+                this.arraySize = parts;
+                const last = code.length - 1;
+                setTimeout(() => {
+                    this.arraySize[last] =
+                        this.arraySize[last] &&
+                        this.arraySize[last].slice(0, 1);
+                    this.$forceUpdate();
+                    this.$refs["otp" + (this.arraySize.length - 1)][0].focus();
+                }, 0);
+            }
+        },
+        computed: {
+            listeners() {
+                return {
+                    ...this.$listeners
+                };
+            }
+        }
+    };
+</script>
+<style scoped>
+    #wrapper {
+        width: 100%;
+        margin: 8px auto 2px;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+    #wrapper input {
+        margin: 0 5px;
+        text-align: center;
+        line-height: 30px;
+        font-size: 35px;
+        border: solid 1px #ccc;
+        box-shadow: 0 0 5px #ccc inset;
+        outline: none;
+        width: 40px;
+        -webkit-transition: all 0.2s ease-in-out;
+        transition: all 0.2s ease-in-out;
+        border-radius: 3px;
+    }
+
+    #wrapper input::-moz-selection {
+        background: transparent;
+    }
+    #wrapper input::selection {
+        background: transparent;
+    }
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    input[type="tel"] {
+        -moz-appearance: textfield;
+    }
+</style>
