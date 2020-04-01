fcf4cf37226f6bce3cc331e122027aeba7641688
djtapurv <apurv.singh@djtcorp.in>
2020-04-01 21:50:01 +0530

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
     <div id="app">
-        <vue-otp-box :numberOfBoxes="4" @inputValue="emitInput" />
+        <vue-otp-box
+            :numberOfBoxes="4"
+            @inputValue="emitInput"
+            @clearValue="clearValue"
+            :clearInput="false"
+        />
     </div>
 </template>
 
 <script>
-    import VueOtpBox from "./components/VueOtpBox.vue";
+import VueOtpBox from "./components/VueOtpBox.vue";
 
-    export default {
-        name: "App",
-        components: {
-            VueOtpBox
+export default {
+    name: "App",
+    components: {
+        VueOtpBox
+    },
+    data() {
+        return {
+            otp: undefined
+        };
+    },
+    methods: {
+        emitInput(value) {
+            this.otp = value;
         },
-        data() {
-            return {
-                otp: undefined
-            };
-        },
-        methods: {
-            emitInput(value) {
-                this.otp = value;
-            }
+        clearValue() {
+            this.otp = "";
         }
-    };
+    }
+};
 </script>
